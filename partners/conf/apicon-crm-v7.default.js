2170410a667b8a2adbc353b96c6436f1f214c5e4
Chris Wiechmann <cwiechmann@axway.com>
2020-10-08 16:04:04 +0200

    Improved APIs

---
+++ partners/conf/apicon-crm-v7.default.js
@@ -0,0 +1,17 @@
+module.exports = {
+	// The configuration settings for the OAS2 flow-node: CRM
+	pluginConfig: {
+		'@axway/api-builder-plugin-fn-swagger': {
+			'apicon-crm-v7': {
+				// It is possible to override URI options when constructing
+				// outbound requests to this service.
+				uri: {
+					// protocol: 'https',
+					// host: 'hostname',
+					// port: 443,
+					// basePath: '/api'
+				}
+			}
+		}
+	}
+};
